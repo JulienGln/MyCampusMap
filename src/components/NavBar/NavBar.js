0fc034f641ec32597baebbdfc739a759cb919499
JulienGln <juliengalerne.jg@gmail.com>
2023-09-26 10:48:53 +0200

    add icons navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -4,26 +4,55 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import MainMap from "../MapView/MainMap";
 import ParameterView from "../Parametres/ParameterView";
 import { Text } from "react-native";
+// https://icons.expo.fyi/Index pour choix des icones
+import { MaterialIcons, FontAwesome5, Ionicons } from "@expo/vector-icons";
 
 const Tab = createBottomTabNavigator();
+
+/* Pour le focus sur l'icone
+options={({ route }) => ({
+          tabBarLabel: ({ focused }) =>
+            focused ? <Text>Paramètres</Text> : <Text></Text>,
+        })}
+      */
 
 export default function NavBar() {
   return (
     <Tab.Navigator
       screenOptions={{
-        tabBarActiveTintColor: "tomato",
-        tabBarInactiveTintColor: "gray",
+        tabBarActiveTintColor: "tomato", // tomato
+        tabBarInactiveTintColor: "gray", //gray
       }}
     >
-      <Tab.Screen name="Carte" component={MainMap} />
-      <Tab.Screen name="Accueil" component={MainMap} />
+      <Tab.Screen
+        name="Carte"
+        component={MainMap}
+        options={{
+          tabBarLabel: "Carte",
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome5 name="map-marker-alt" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Accueil"
+        component={MainMap}
+        options={{
+          tabBarLabel: "Accueil",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons name="home" color={color} size={size} />
+          ),
+        }}
+      />
       <Tab.Screen
         name="Paramètres"
         component={ParameterView}
-        options={({ route }) => ({
-          tabBarLabel: ({ focused }) =>
-            focused ? <Text>Paramètres</Text> : <Text></Text>,
-        })}
+        options={{
+          tabBarLabel: "Paramètres",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="settings" size={size} color={color} />
+          ),
+        }}
       />
     </Tab.Navigator>
   );
