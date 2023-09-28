fdfc079db01d05ef814e518bfd25598e2ac3c09d
JulienGln <juliengalerne.jg@gmail.com>
2023-09-28 18:12:04 +0200

    Début du thème sombre

--- src/components/Parametres/ParameterView.js
+++ src/components/Parametres/ParameterView.js
@@ -6,9 +6,29 @@ import {
   Appearance,
   StyleSheet,
   ScrollView,
+  ActivityIndicator,
+  Switch,
 } from "react-native";
 
 export default function ParameterView() {
+  const [isSwitchThemeEnabled, setSwitchThemeEnabled] = useState(false);
+  const [theme, setTheme] = useState(Appearance.getColorScheme().toString()); // thème par défaut de l'OS
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: theme === "light" ? "white" : "black", // gestion du thème de l'OS pour affichage cohérent
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    scrollView: {
+      flex: 1,
+    },
+    text: {
+      color: theme === "light" ? "black" : "white",
+    },
+  });
+
   async function communicationServer() {
     // doc : https://reactnative.dev/docs/network
     //var url = "http://192.168.1.23:3000/data"; // adresse IP de l'ordinateur qui fait tourner le serveur
@@ -38,32 +58,115 @@ export default function ParameterView() {
     fetchData();
   }, []);
 
+  function toggleTheme() {
+    setSwitchThemeEnabled(!isSwitchThemeEnabled);
+    setTheme(theme === "light" ? "dark" : "light");
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.text}>OS : {Platform.OS.toLocaleUpperCase()}</Text>
       <Text style={styles.text}>Version : {Platform.Version}</Text>
       <Text style={styles.text}>
-        Thème (par défaut) : {Appearance.getColorScheme().toLocaleUpperCase()}
+        Thème (par défaut) : {theme.toLocaleUpperCase()}
+        {"\n"}
+        <Switch
+          style={{ alignItems: "center", justifyContent: "center" }}
+          onValueChange={toggleTheme}
+          value={isSwitchThemeEnabled}
+        ></Switch>
       </Text>
       <ScrollView style={styles.scrollView}>
-        <Text style={styles.text}> JSON : {JSON.stringify(data)}</Text>
+        {data ? (
+          <Text style={styles.text}> JSON : {JSON.stringify(data)}</Text>
+        ) : (
+          <Text style={styles.text}>
+            JSON :
+            <ActivityIndicator size="large" />
+          </Text>
+        )}
       </ScrollView>
     </View>
   );
 }
 
-const colorTheme = Appearance.getColorScheme() === "light" ? "white" : "black"; // gestion du thème de l'OS pour affichage cohérent
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: colorTheme,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  scrollView: {
-    flex: 1,
-  },
-  text: {
-    color: colorTheme === "white" ? "black" : "white",
-  },
-});
+/** 
+Pour appliquer le choix du mode sombre aux autres composants dans React Native, vous pouvez utiliser le `Context` de React. Le `Context` fournit un moyen de passer des données à travers l'arborescence du composant sans avoir à passer les props manuellement à chaque niveau.
+
+Voici un exemple de la façon dont vous pouvez le faire :
+
+1. Créez un `DarkModeContext` :
+
+```jsx
+import React from 'react';
+
+export const DarkModeContext = React.createContext();
+```
+
+2. Utilisez ce contexte dans votre composant principal (généralement `App.js`) pour stocker l'état du mode sombre :
+
+```jsx
+import { DarkModeContext } from './DarkModeContext';
+
+function App() {
+  const [darkMode, setDarkMode] = React.useState(false);
+
+  return (
+    <DarkModeContext.Provider value={{ darkMode, setDarkMode }}>
+      {Vos autres composants vont ici }
+      </DarkModeContext.Provider>
+      );
+    }
+    
+    export default App;
+    ```
+    
+    3. Ensuite, dans votre `ParameterView.js`, vous pouvez utiliser `setDarkMode` pour changer l'état lorsque l'utilisateur bascule le switch :
+    
+    ```jsx
+    import { DarkModeContext } from './DarkModeContext';
+    
+    function ParameterView() {
+      const { setDarkMode } = React.useContext(DarkModeContext);
+    
+      const handleSwitchChange = (value) => {
+        setDarkMode(value);
+      };
+    
+      return (
+        <Switch onValueChange={handleSwitchChange} />
+      );
+    }
+    
+    export default ParameterView;
+    ```
+    
+    4. Enfin, dans n'importe quel autre composant où vous voulez utiliser le mode sombre, vous pouvez accéder à `darkMode` à partir du contexte et l'utiliser pour déterminer vos styles :
+    
+    ```jsx
+    import { DarkModeContext } from './DarkModeContext';
+    
+    function SomeOtherComponent() {
+      const { darkMode } = React.useContext(DarkModeContext);
+    
+      const textStyle = darkMode ? styles.darkText : styles.lightText;
+    
+      return (
+        <Text style={textStyle}>Hello, world!</Text>
+      );
+    }
+    
+    const styles = StyleSheet.create({
+      lightText: {
+        color: 'black',
+      },
+      darkText: {
+        color: 'white',
+      },
+    });
+    
+    export default SomeOtherComponent;
+    ```
+    
+    Dans cet exemple, `SomeOtherComponent` utilise la valeur de `darkMode` pour déterminer sa couleur de texte. Vous pouvez adapter ce code à vos besoins spécifiques. J'espère que cela vous aide ! N'hésitez pas si vous avez d'autres questions.
+*/
