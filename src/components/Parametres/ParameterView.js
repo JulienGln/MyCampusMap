5f70da02e9d8656b4f6242c9298ee12230521a9a
JulienGln <juliengalerne.jg@gmail.com>
2023-10-31 13:37:09 +0100

    Asset Dark/Light Mode dans Paramètres

--- src/components/Parametres/ParameterView.js
+++ src/components/Parametres/ParameterView.js
@@ -3,11 +3,13 @@ import {
   Text,
   View,
   Platform,
-  Appearance,
+  TouchableOpacity,
+  Image,
   StyleSheet,
   ScrollView,
   ActivityIndicator,
   Switch,
+  Dimensions,
 } from "react-native";
 
 import { getAppTheme, setAppTheme } from "../../helpers/localStorage";
@@ -16,6 +18,9 @@ import { ThemeContext } from "../../themeContext";
 export default function ParameterView() {
   const [isSwitchThemeEnabled, setSwitchThemeEnabled] = useState(false);
   const { theme, setTheme } = useContext(ThemeContext);
+
+  const windowWidth = Dimensions.get("window").width;
+  const windowHeight = Dimensions.get("window").height;
 
   const styles = StyleSheet.create({
     container: {
@@ -81,6 +86,32 @@ export default function ParameterView() {
           value={isSwitchThemeEnabled}
         ></Switch>
       </Text>
+
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-around",
+          padding: 10,
+        }}
+      >
+        {theme === "dark" && (
+          <TouchableOpacity onPress={toggleTheme}>
+            <Image
+              source={require("../../assets/light_mode.png")}
+              style={{ width: 120, height: 120, margin: 10 }}
+            />
+          </TouchableOpacity>
+        )}
+        {theme === "light" && (
+          <TouchableOpacity onPress={toggleTheme}>
+            <Image
+              source={require("../../assets/dark_mode.png")}
+              style={{ width: 120, height: 120, margin: 10, borderRadius: 100 }}
+            />
+          </TouchableOpacity>
+        )}
+      </View>
+
       <ScrollView style={styles.scrollView}>
         {data ? (
           <Text style={styles.text}> JSON : {JSON.stringify(data)}</Text>
